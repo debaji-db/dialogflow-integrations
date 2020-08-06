221105b1b1dd9f8a8f77d3c1676a66d9544799d2
debaji-db <69307770+debaji-db@users.noreply.github.com>
2020-08-06 18:25:23 +0530

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'fbi-bot-lwup';
+const phoneNumber = "+14155238886";
+const accountSid = 'AC78785eb8187e016d3fa065adb1f08e18';
+const authToken = '75a5ebf8b4cfa8d6041a348db446724e';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
